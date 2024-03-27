836ed47ae7040c1222f4e179dacc9073d041393f
Daniel Koch <daniel.koch@helsinki.fi>
2024-03-27 16:03:02 +0200

    /bos homepage with react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ReactFlowProvider } from 'reactflow';
+import { BrowserRouter } from 'react-router-dom';
  
 import App from './App';
  
@@ -10,8 +11,10 @@ console.log("This is running in the main.tsx file");
  
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+  <BrowserRouter basename="/bos">
     <ReactFlowProvider>
       <App />
     </ReactFlowProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 );
