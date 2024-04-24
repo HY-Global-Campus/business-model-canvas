d14bd2a1eb05110cd320468ecfb9ad00043c3b47
Daniel Koch <daniel.koch@helsinki.fi>
2024-04-24 13:02:25 +0300

    fix font

--- frontend/src/App/index.tsx
+++ frontend/src/App/index.tsx
@@ -3,7 +3,6 @@ import Mindmap from "./Pages/Mindmap"
 import FrontPage from './Pages/Frontpage';
 import ExercisePage from './Pages/ExercisePage';
 import gothamNarrow from '../assets/Gotham-Narrow-Font-Family/GothamNarrow-Book.otf'
-import { CSSProperties } from 'react';
 
 function App() {
 const myFontFace = `
@@ -13,23 +12,24 @@ const myFontFace = `
     font-weight: normal;
     font-style: normal;
   }
+  body {
+  font-family: 'Gotham Narrow', sans-serif;
+}
 `;
 
-const textStyle: CSSProperties = {
-  fontFamily: "'Gotham Narrow', sans-serif",
-};  
+
   return (
     <>
     <style>
       {myFontFace}
     </style>
-    <div style={textStyle}>
+
     <Routes>
       <Route path="/" element={<FrontPage />} />
       <Route path="/mindmap" element={ <Mindmap />} />
       <Route path="/exercise" element= {<ExercisePage />} />
     </Routes>
-    </div>
+
     </>
   );
 }
