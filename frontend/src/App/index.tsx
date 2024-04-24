de47c2e13a64df2ae6ee9ef09f58ca41098f6a07
Daniel Koch <daniel.koch@helsinki.fi>
2024-04-24 12:49:13 +0300

    change font

--- frontend/src/App/index.tsx
+++ frontend/src/App/index.tsx
@@ -2,14 +2,35 @@ import { Routes, Route } from 'react-router-dom';
 import Mindmap from "./Pages/Mindmap"
 import FrontPage from './Pages/Frontpage';
 import ExercisePage from './Pages/ExercisePage';
+import gothamNarrow from '../assets/Gotham-Narrow-Font-Family/GothamNarrow-Book.otf'
+import { CSSProperties } from 'react';
 
 function App() {
+const myFontFace = `
+  @font-face {
+    font-family: 'Gotham Narrow';
+    src: url(${gothamNarrow}) format('opentype');
+    font-weight: normal;
+    font-style: normal;
+  }
+`;
+
+const textStyle: CSSProperties = {
+  fontFamily: "'Gotham Narrow', sans-serif",
+};  
   return (
+    <>
+    <style>
+      {myFontFace}
+    </style>
+    <div style={textStyle}>
     <Routes>
       <Route path="/" element={<FrontPage />} />
       <Route path="/mindmap" element={ <Mindmap />} />
       <Route path="/exercise" element= {<ExercisePage />} />
     </Routes>
+    </div>
+    </>
   );
 }
 
