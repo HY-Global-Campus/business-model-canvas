a5f39e214cb7f9f68561f258868a069ab18f03dc
Daniel Koch <daniel.koch@helsinki.fi>
2024-04-24 12:03:17 +0300

    vite conf

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/bos'
+  base: '/bos',
+  assetsInclude: ['**/*.png', '**/*.svg']
 })
