176954e535f9e02e875f201883d72935f45e056d
Daniel Koch <daniel.koch@helsinki.fi>
2024-06-13 13:44:54 +0300

    modify header style, add icon

--- frontend/src/App/Components/Header.tsx
+++ frontend/src/App/Components/Header.tsx
@@ -1,5 +1,7 @@
 import hyLogoWhite from '../../assets/HY-Logo_White.svg'
+import borealforest_icon from '../../assets/Icon1_Milton_O.png'
 import borealforest from '../../assets/HY_Serendip-BOREALFOREST.jpg'
+import { CSSProperties } from 'react';
 
 const Header: React.FC = () => {
     const headerStyle: React.CSSProperties = {
@@ -22,9 +24,26 @@ const Header: React.FC = () => {
         marginRight: '10px', // Adjust the spacing as needed
     };
 
+    const iconstyle: CSSProperties = {
+        height: '60px',
+        width: 'auto',
+        position: 'relative',
+        top: '25px'
+    }
+
+    const titlestyle: CSSProperties = {
+        display: 'flex',
+        justifyContent: 'flex-start',
+        flexDirection: 'row',
+        fontSize: '25px'
+    }
+
     return (
         <header style={headerStyle}>
-            <h1 style={titleStyle}>Book of Serendip</h1>
+            <div style={titlestyle}>
+                <img src={borealforest_icon} alt='Boreal forest icon' style={iconstyle} />
+                <h1 style={titleStyle}>Book of Serendip</h1>
+            </div>
             <img src={hyLogoWhite} alt="Logo" style={logoStyle} />
         </header>
     );
