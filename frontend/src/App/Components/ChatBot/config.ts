bfd18325e3b82a82940259e552ec1e896623892a
Daniel Koch <daniel.koch@helsinki.fi>
2024-08-28 20:14:03 +0300

    fix eslint error

--- frontend/src/App/Components/ChatBot/config.ts
+++ frontend/src/App/Components/ChatBot/config.ts
@@ -1,6 +1,6 @@
 import { createChatBotMessage } from 'react-chatbot-kit';
 const config = {
-	initialMessages: [createChatBotMessage('Hello! I am Madida, your personal assistant.')],
+	initialMessages: [createChatBotMessage(`Hello! I am Madida and I here to help you.`, {})],
 	botName: 'Madida',
 	state: {
 		gptChatHistory: [],
