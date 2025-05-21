ddb580454d45ec07cda370b421d7fb1879be521d
Daniel Koch <daniel.koch@helsinki.fi>
2025-05-21 13:48:42 +0300

    debug message

--- backend/src/controllers/serendip-mooc-whitelist.ts
+++ backend/src/controllers/serendip-mooc-whitelist.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from 'express';
-import {getCloudSave, updateCloudSave} from '../services/accelbyte.js';
+import { getCloudSave, updateCloudSave } from '../services/accelbyte.js';
 import config from '../config.js';
 const router = Router();
 
@@ -8,24 +8,27 @@ router.get('/', async (req: Request, res: Response) => {
     const expectedToken = `Bearer ${config.SERENDIP_WHITELIST_TOKEN}`.trim();
     if (authHeader !== expectedToken) {
         console.log("Unauthorized, the token was: ", authHeader, "' expected: ", expectedToken);
-        res.status(401).json({message: 'Unauthorized'});
+        res.status(401).json({ message: 'Unauthorized' });
         return;
     }
-    console.log("Auth successfull, trying to access cloudsace");
+    console.log("Auth successfull, trying to access cloudsave");
     let result = await getCloudSave('permissions');
     console.log("result", result);
-    
+
     res.json(result);
 });
 
-router.post ('/', async (req: Request, res: Response) => {
-    if (req.headers.authorization !== `Bearer ${config.SERENDIP_WHITELIST_TOKEN}`) {
-        res.status(401).json({message: 'Unauthorized'});
+router.post('/', async (req: Request, res: Response) => {
+    const authHeader = req.headers.authorization?.trim();
+    const expectedToken = `Bearer ${config.SERENDIP_WHITELIST_TOKEN}`.trim();
+    if (authHeader !== expectedToken) {
+        console.log("Unauthorized, the token was: ", authHeader, "' expected: ", expectedToken);
+        res.status(401).json({ message: 'Unauthorized' });
         return;
     }
     console.log("req.body", req.body)
     if (!req.body) {
-        res.status(400).json({message: 'Missing permissions data'});
+        res.status(400).json({ message: 'Missing permissions data' });
         return;
     }
     let old = await getCloudSave('permissions');
@@ -35,7 +38,7 @@ router.post ('/', async (req: Request, res: Response) => {
         let result = await updateCloudSave('permissions', old.value);
         res.json(result);
     } else {
-        res.status(500).json({message: 'Failed to get permissions'});
+        res.status(500).json({ message: 'Failed to get permissions' });
     }
 });
 
