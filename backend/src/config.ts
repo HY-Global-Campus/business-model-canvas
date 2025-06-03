b82b13c3141655767f2d8bd8e467434ad8886d27
Daniel Koch <daniel.koch@helsinki.fi>
2025-06-03 09:30:50 +0300

    trim env var, remove unused env var

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -4,7 +4,6 @@ interface Config {
   JWT_SECRET: string,
   DB_URL: string,
   PORT: number,
-  ACCELBYTE_TOKEN: string,
   GCAI_URL: string,
   GCAI_TOKEN: string,
   ACCELBYTE_ADMIN_SECRET: string,
@@ -16,12 +15,11 @@ const config: Config = {
   JWT_SECRET: process.env.JWT_SECRET!.trim(),
   DB_URL: process.env.DATABASE_URL!.trim(),
   PORT: Number(process.env.PORT) || 80,
-  ACCELBYTE_TOKEN: process.env.ACCELBYTE_TOKEN!.trim(),
   GCAI_URL: process.env.GCAI_URL!.trim(),
   GCAI_TOKEN: process.env.GCAI_TOKEN!.trim(),
   ACCELBYTE_ADMIN_SECRET: process.env.ACCELBYTE_ADMIN_SECRET!.trim(),
   ACCELBYTE_ADMIN_CLIENT_ID: process.env.ACCELBYTE_ADMIN_CLIENT_ID!.trim(),
-  SERENDIP_WHITELIST_TOKEN: process.env.SERENDIP_WHITELIST_TOKEN.trim()!
+  SERENDIP_WHITELIST_TOKEN: process.env.SERENDIP_WHITELIST_TOKEN!.trim()
 }
 
 export default config;
